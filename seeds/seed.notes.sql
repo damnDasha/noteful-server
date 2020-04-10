127a736e756b7ddeb595f1b92f3cea7216994da4
dasha akhten <dasha.akhten@gmail.com>
2020-04-09 22:58:56 -0700

    maybe maybe maybe

--- seeds/seed.notes.sql
+++ seeds/seed.notes.sql
@@ -1,7 +1,8 @@
 INSERT INTO notes (id, title, content, date, folder_id)
 VALUES
-    (1, 'Note 1', 'note 1  Content', '2029-01-22T16:28:32.615Z', 1),
-    (2, 'Note 2', 'note 2 Content2', '2029-01-22T16:28:32.615Z', 1),
-    (3, 'Note 3', 'note 3 bah content Content3', '2029-01-22T16:28:32.615Z', 2)
+    (1, 'Note 1', 'note 1  Content', '2009-01-22T16:28:32.615Z', 1),
+    (2, 'Note 2', 'note 2 Content2', '2009-01-22T16:28:32.615Z', 2),
+    (3, 'Note 3', 'note 3 bah content Content3', '2009-01-22T16:28:32.615Z', 3)
+    
 
 
